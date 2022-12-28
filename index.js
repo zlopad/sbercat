5786a6e77d95cf47c991bc8144e68bfdd17aa436
Ivan <vanix07@mail.ru>
2022-12-29 01:58:35 +0300

    Init Coni2t

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 console.log('work')
-
 const $wr = document.querySelector('[data-wr]')
+
+const $modalWr = document.querySelector('[data-modalWr]')
+const $modalContent = document.querySelector('[data-modalContent]')
+const $catCreateFormTemplate = document.getElementById('createCatForm')
+
+const CREATE_FORM_LS_KEY = 'CREATE_FORM_LS_KEY'
 
 const ACTIONS = {
   DETAIL: 'detail',
@@ -21,7 +26,7 @@ const getCatHTML = (cat) => `
 	</div>
 	`
 
-fetch('https://cats.petiteweb.dev/api/single/Zlopad/show/')
+fetch('https://cats.petiteweb.dev/api/single/zlopad/show/')
   .then((res) => res.json())
   .then((data) => {
     $wr.insertAdjacentHTML(
@@ -44,5 +49,92 @@ $wr.addEventListener('click', (e) => {
 
       alert(`Удаление кота с id = ${catId} не удалось`)
     })
+  }
+})
+
+const formatCreateFormData = (formDataObject) => ({
+  ...formDataObject,
+  id: +formDataObject.id,
+  rate: +formDataObject.rate,
+  age: +formDataObject.age,
+  favorite: !!formDataObject.favorite,
+})
+
+const clickModalWrHandler = (e) => {
+  if (e.target === $modalWr) {
+    $modalWr.classList.add('hidden')
+    $modalWr.removeEventListener('click', clickModalWrHandler)
+    $modalContent.innerHTML = ''
+  }
+}
+
+const openModalHandler = (e) => {
+  const targetModalName = e.target.dataset.openmodal
+
+  if (targetModalName === 'createCat') {
+    $modalWr.classList.remove('hidden')
+    $modalWr.addEventListener('click', clickModalWrHandler)
+
+    const cloneCatCreateForm = $catCreateFormTemplate.content.cloneNode(true)
+    $modalContent.appendChild(cloneCatCreateForm)
+
+    const $createCatForm = document.forms.createCatForm
+
+    const dataFromLS = localStorage.getItem(CREATE_FORM_LS_KEY)
+
+    const preparedDataFromLS = dataFromLS && JSON.parse(dataFromLS)
+
+    if (preparedDataFromLS) {
+      Object.keys(preparedDataFromLS).forEach((key) => {
+        $createCatForm[key].value = preparedDataFromLS[key]
+      })
+    }
+
+    $createCatForm.addEventListener('submit', (submitEvent) => {
+      submitEvent.preventDefault()
+
+      const formDataObject = formatCreateFormData(
+        Object.fromEntries(new FormData(submitEvent.target).entries()),
+      )
+
+      fetch('https://cats.petiteweb.dev/api/single/ZholobovSS/add/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataObject),
+      }).then((res) => {
+        if (res.status === 200) {
+          $modalWr.classList.add('hidden')
+          $modalWr.removeEventListener('click', clickModalWrHandler)
+          $modalContent.innerHTML = ''
+
+      
+          localStorage.removeItem(CREATE_FORM_LS_KEY)
+          return $wr.insertAdjacentHTML(
+            'afterbegin',
+            getCatHTML(formDataObject),
+          )
+        }
+        throw Error('Ошибка при создании кота')
+      }).catch(alert)
+    })
+    $createCatForm.addEventListener('change', () => {
+      const formattedData = formatCreateFormData(
+        Object.fromEntries(new FormData($createCatForm).entries()),
+      )
+
+      localStorage.setItem(CREATE_FORM_LS_KEY, JSON.stringify(formattedData))
+    })
+  }
+}
+
+document.addEventListener('click', openModalHandler)
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    $modalWr.classList.add('hidden')
+    $modalWr.removeEventListener('click', clickModalWrHandler)
+    $modalContent.innerHTML = ''
   }
 })
